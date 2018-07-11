13f98b31681ba1703c063cb3ed2a9105137b2b66
presto <paul@prestopnik.com>
2018-07-10 22:46:42 -0600

    updates to lesson 6 & 7

---
+++ 6/spam.py
@@ -0,0 +1,38 @@
+
+# code is often organized into functions.  A function is a little bit of code
+# that can be reused over and over.  It's given a name, and you just use the
+# name of the function to make the program run the code that is in the function.
+
+# You can identify functions, because they have parenthesis when they are used.
+# We've used two functions so far.
+
+# The first was print()
+# The second function we used was input()
+
+# let's remember how those work
+
+# See if you can figure out what these next two lines will do, then
+# run this program and see if you were correct.
+name = input("Please enter your name: ")
+print "Hello " + name
+
+
+# python has lots of functions you can use, but you can also write your own function.
+def spam()
+  print "Lovely spam! Wonderful spam!"
+
+
+# now that we've written that function, we can "call" the function.
+# try uncommenting this line to see what it does
+
+# spam()
+
+# try calling the function several times, to see that it calls the function each time.
+
+
+
+
+
+
+
+
---
+++ 7/birthday.py
@@ -0,0 +1,28 @@
+# this time we are going to write a function that will take an "argument"
+
+# an argument is a value you give to a function to change it's behavior
+
+# we are going to write a function that sings happy birthday
+
+# it has an argument called "name", and this is the name of the person
+# we are singing to.
+def happyBirthday(birthday_name):
+    print "Happy Birthday to you."
+    print "Happy Birthday to you."
+    print "Happy Birthday dear " + birthday_name
+    print "Happy Birthday to you."
+
+
+# now we can call the function:
+name = "John Cleese"
+happyBirthday(name)
+
+# Can you guess what will happen when you run this program?
+# try it out!
+
+
+# Now can you change this function so that it sings happy birthday
+# to someone else?
+
+# Can you modify it again so that it asks the user for their name
+# and sings happy birthday to them?
