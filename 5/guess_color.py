c6e785a962af99fdf4f7fac5b7f0b55825b7ee28
Ozyman <paul@prestopnik.com>
2022-04-15 11:36:17 -0600

    Update guess_color.py
    additional comments

--- 5/guess_color.py
+++ 5/guess_color.py
@@ -4,8 +4,16 @@
 color_guess = input("Can you guess the color I am thinking of? ")
 
 # then if they didn't guess right, we keep giving them more tries until they get it right.
+# because we want to do the same thing over and over again we are using a 'loop'
+# the 'while loop' keeps doing the same thing *while* the "condition" is true
+# A "condition" is something that is true or false.
+
+# != means "not equal", so these lines will keep asking the user to guess a color while their guess is not 'blue'
+# just like the 'if' statement, the lines that go with the 'while' statement are indented to show they belong to it.
 while color_guess != "blue":
     color_guess = input("That's not it.  Guess again! ")
+    
+
 
 print("You guessed right!")
 
