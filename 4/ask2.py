72a53a61b75802f2d815dbd535a3f73b35fb8531
Ozyman <paul@prestopnik.com>
2022-04-26 20:09:07 -0600

    added if/else and """/''''

--- 4/ask2.py
+++ 4/ask2.py
@@ -13,7 +13,38 @@ if favorite_color == "blue":
 else:
     print("I prefer blue.")
 
-# Can you add responses for other colors?
+# go ahead and run this program now to see how it works.  Try running it a couple times and try giving different
+# answers.  Can you get the program to give both responses?
+
+# Once you've run the program a few times, you are going to modify it.
+
+# If their favorite color isn't blue, you can add another if statement.  
+# You'll have to indent the new 'if' under the else statement.  It should look like this:
+
+"""
+  if (condition1):
+    do_thing()
+  else:
+    if (condition2):
+        do_other_thing()
+    else:
+        do_third_thing()
+"""
+
+# You may have noticed that this example code doesn't have a # sign at the beginning, 
+# but it still  doesn't do anything when the program is run.
+# Having three quotes all together like shown above can be used to start/end a kind block that 
+# acts like a multi-line comment.
+
+
+'''
+You can use normal quotes (") like above or single quotes ('),
+like this example.  They work the pretty much the same.
+''' 
+
+ 
+# Now modify the code to give a different response for another color using the if, else/if, else structure
+# 
 # Can you ask the user another question, and say something in response?
 # maybe ask how old they are.  Or if they like some TV show or what their favorite movie is.
 
