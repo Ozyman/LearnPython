4ee36c2d651a59fb2f8d72ed0b8e684057129ab1
Ozyman <paul@prestopnik.com>
2022-06-12 12:04:21 -0600

    more improvements to comments

--- 4/ask2.py
+++ 4/ask2.py
@@ -1,5 +1,5 @@
 # We are going to ask the user a question and put their answer into a variable
-#favorite_color = input("What is your favorite color? ")
+favorite_color = input("What is your favorite color? ")
 
 
 # Now we want to respond in different ways depending on how the user answered our question.
@@ -11,23 +11,68 @@
 
 # After the lines with 'if' and 'else', the next line is indented to show that it goes with the if/else line above.
 
-#if favorite_color == "blue" or favorite_color == "Blue" or favorite_color == "Blue." or favorite_color == "blue.":
-  #  print("That's my favorite too!")
-#else:
-  #  print("I prefer blue.")
+if favorite_color == "blue":
+    print("That's my favorite too!")
+else:
+    print("I prefer blue.")
 
 # go ahead and run this program now to see how it works.  Try running it a couple times and try giving different
 # answers.  Can you get the program to give both responses?
 
+# If you didn't already try typing "Blue" (With a capitol 'B'), try that.  Why does the program respond the way it does?
+
 # After you've run the program a few times, then you are going to modify it.
+# You are going to add a condition so that it also works with a capital 'B' -> "Blue"
 
-# If the user's favorite color isn't blue, you can add another if statement to check another color. 
-# To do this we will use a new key word: 'elsif'
-# This acts like an else combined with another 'if'
+# To do this we want the conditional to check if the favorite_color is "blue" or "Blue".
+# Change the 'if' statement on line 14 above to look like this and rerun the program:
+# if favorite_color == "blue" or favorite_color == "Blue":
 
+# notice that this next line would not work:
+# if favorite_color == "blue" or "Blue":
+
+# 'or' can only be used to join two conditionals that are complete and independent.  
+# It can not be used to compare one variable against two different things.
+
+# You can even add a third (or more conditions) like this:
+# if favorite_color == "blue" or favorite_color == "Blue" or favorite_color == "BLUE":
+
+
+
+# If the user's favorite color isn't blue, you can add additional if statement to check another color. 
 # This example code below says if condition1 is true, then do_thing().  If it's not true than check if condition2 is true.
 # If condition1 was false and condition2 was true, than do_other_thing().
 # Finally if both condition1 was false, and condition2 was also false, do_third_thing()
+'''
+if (condition1):
+  do_thing()
+else: 
+  if (condition2):
+    do_other_thing()
+  else:
+    do_third_thing()
+'''
+
+# Let's take a break from if & else for a second...
+# You may have noticed that this example code doesn't have a # sign at the beginning, 
+# but it still doesn't do anything when the program is run.
+# Having three quotes all together like shown above can be used to start/end a block that 
+# acts like a multi-line comment.
+
+# Usually you just use a '#' on each line, even if you have a multi line comment,
+# but sometimes it can be easier to add a ''' before and after several lines to turn the entire block into a comment.
+
+'''
+You can use normal quotes (") like above or single quotes ('),
+like this example.  They work the pretty much the same.
+''' 
+
+
+
+# Back to the if & else stuff...
+# The pattern of using another if statement after an else is so common, that it has a shortcut.
+# 'elif' acts like an 'else' combined with another 'if'
+# This code below does the same thing as the code above
 """
   if (condition1):
     do_thing()
@@ -37,30 +82,10 @@
     do_third_thing()
 """
 
-# You may have noticed that this example code doesn't have a # sign at the beginning, 
-# but it still doesn't do anything when the program is run.
-# Having three quotes all together like shown above can be used to start/end a kind block that 
-# acts like a multi-line comment.
+
+# Now that you've learned about if & else - go back to the original code, and modify it
+# to give a different response for another color using the if, elif, else structure
 
 
-'''
-You can use normal quotes (") like above or single quotes ('),
-like this example.  They work the pretty much the same.
-''' 
-
- 
-# Now modify the code to give a different response for another color using the if, elif, else structure
-# 
-# Can you ask the user another question, and say something in response?
-# maybe ask how old they are.  Or if they like some TV show or what their favorite movie is.
-
-atla = input("Who's your favorite ATLA character? ")
-
-if atla == "Toph" or atla == "Toph" or atla == "TOPH!":
-    print("mine too! I love her! 'I AM THE MELON KING!' HehEehE")
-elif atla == "Zuko":
-    print('Oh, hes my second favorite!')
-
-else:
-    print("Oh! That's a cool character too! But I like toph better. Then zuko.")
+# If you got all that working, congratulations!  Go check out the ask2-challenge.py for some additional practice.
 
--- 5/guess_color.py
+++ 5/guess_color.py
@@ -1,22 +1,32 @@
 # Now we are going to try a guessing game
 
-# We ask the user to guess a color.
-
+# First, We ask the user to guess a color.
 color_guess = input("Can you guess the color I am thinking of? ")
 
 # then if they didn't guess right, we keep giving them more tries until they get it right.
-# because we want to do the same thing over and over again we are using a 'loop'
-# the 'while loop' keeps doing the same thing *while* the "condition" is true
-# A "condition" is something that is true or false.
+# because we want to do the same thing over and over again we are using a loop
+# the "while" loop keeps doing something *while* the "condition" is true
+# A "condition" is a statement that is true or false.  
+# For example, in the 'ask2.py', we used this condition:
+# favorite_color == "blue"
 
-# != means "not equal", so these lines will keep asking the user to guess a color while their guess is not 'blue'
-# just like the 'if' statement, the lines that go with the 'while' statement are indented to show they belong to it.
+# That condition checks if two things are equal, but a condition can also check if two things are unequal.
+# Just like == means "are these things equal?".  != means "not equal"
+# 
+# These next lines will check to see if color_guess is blue.  The loop will continue looping forever, while color_guess is not equal to "blue".
+# just like the 'if' statement, the lines that go with the 'while' statement are indented to show they belong to it.  (The indended lines are called the "body" of the loop)
+# In the "body" of the loop, the program asks the user to guess again, and gives them another chance.
+# After asking them for a new guess, the while condition is checked again.  If color_guess is still not equal to "blue", then the user gets asked again, and the answer gets checked again, and so on forever...
+# Or at least *while* the condition is true. 
+# In this case the condition is true when color_guess is *not* equal to "blue".
+
 while color_guess != "blue":
     color_guess = input("That's not it.  Guess again! ")
-    
 
-
+# If color_guess was equal to "blue", the condition would be false, and the while loop would "exit".  It would no longer go into the "body", and instead continues after the loop, and tells them they guessed right
 print("You guessed right!")
+
+# Try running the program.  Enter a few wrong answers, then enter "blue".
 
 # What if the user enters "Blue" (i.e. with a capital 'B') does that work?
 # go try it...
@@ -31,7 +41,10 @@ print("You guessed right!")
 #
 # computers are stupid - you have to tell them *exactly* what to do
 
-# Try to change the color that we want the user to guess.
-# Change the program to guess something else.  Maybe a number or name of your best friend.
+# Change the message that is printed when they guess the correct color.  Tell them. "How did you know I was thinking of blue?"
+# Run the program and make sure your change worked.
+
+# Change the color that we want the user to guess.  Make sure to also change the color mentioned when they guess correctly.
+
 
 
