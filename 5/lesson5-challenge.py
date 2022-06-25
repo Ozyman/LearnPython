9c82d819da7091a4baa5f69364816e5f6f1a00c9
Ozyman <paul@prestopnik.com>
2022-06-24 22:31:53 -0600

    initial commit

---
+++ 5/lesson5-challenge.py
@@ -0,0 +1,46 @@
+# This script will:
+# 1) Ask a user for a word/phrase
+# 2) Ask them how many times it should be printed
+# 3) Print the word/phrase that many times.
+
+# After each line of ###############, write the code as described above that line.
+# Often times it can be useful to only write part of a program, and then to run the program to make sure it is behaving as expected so far.
+# If the line of ########## has (TEST HERE) at the end of it.  You should run the program after that point to see if it is behaving as expected so far.
+# Sometimes a test will not look any different after a new line is added, if the line does not produce output, but if it runs without an error, that is a succesful test at that point!
+
+# First ask the user for the word/phrase and store it in a variable using the input() function  
+# Picking a descriptive name for the variable makes it easier to understand the program as you are writing it and later when you or someone else has to read it.
+# In python variables are usually all lower case, and if they are more than one word, they use a '_' to separate the words.  e.g.  my_beautiful_variable = "hello"
+################################## (TEST HERE)
+
+
+# Now ask the user for the number of times it should be printed, and store that in a different variable
+################################## (TEST HERE)
+
+
+# input() returns a string, but to do arithmetic we need to convert to an integer
+# use int() to convert the last variable to an integer, and store it in a new variable.
+################################## (TEST HERE)
+
+
+# A while loop will be used to count the number of times the phrase is printed, and keep looping until we print enough.
+# First create a variable to keep track of the number of times we've printed it so far.  Set it to zero to start, because at this point we've printed zero times
+################################## (TEST HERE)
+
+
+# Next write a while statement.  The condition in the while statement should check if the number of times we've printed so far is less than the 
+# number of times we want to print.
+##################################
+
+
+# In the body of the while loop (don't forget to indent), we need to do two things.
+# First thing we do is print the phrase from the user
+# ################################
+
+# Next we need to add one to our count that is keeping track of the number of prints.
+# if your variable is called dustin_henderson (a great name for a person, a bad name for this variable), you would say:
+# dustin_henderson = dustin_henderson + 1
+##################################
+
+
+# That's it.  You can now run your program and see if it works!
