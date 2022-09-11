3046c710d1c996ec87495f5c36141e7d69462983
Ozyman <paul@prestopnik.com>
2022-09-10 23:35:12 -0600

    have student write subtraction.

--- 8/lesson8-reading.py
+++ 8/lesson8-reading.py
@@ -3,7 +3,7 @@
 # f-strings are a more convenient way to build strings from variables.
 # This is a simple program to illustrate
 
-# First get two numbers from the user.  Since input() returns a string, we wrap it in a call to int() to immediately conver to an integer
+# First get two numbers from the user.  Since input() returns a string, we wrap it in a call to int() to immediately convert to an integer
 num1 = int(input("please enter a number: "))
 num2 = int(input("please enter another number: "))
 
@@ -11,7 +11,6 @@ print() # blank line to make it neater
 
 # Then print some math facts for these numbers:
 print(str(num1) + " + " + str(num2) + " = " + str(num1+num2))
-print(str(num1) + " - " + str(num2) + " = " + str(num1-num2))
 print(str(num1) + " * " + str(num2) + " = " + str(num1*num2))
 print(str(num1) + " ^ " + str(num2) + " = " + str(num1**num2))
 
@@ -24,7 +23,6 @@ print()  # put a blank line to make things neater
 # You just need to put an f in front of the first quote
 # Then in the string you can put {} with a variable (or an expresion) inside. 
 print(f"{num1} + {num2} = {num1+num2}")
-print(f"{num1} - {num2} = {num1-num2}")
 print(f"{num1} * {num2} = {num1*num2}")
 print(f"{num1} ^ {num2} = {num1**num2}")
 
@@ -32,8 +30,11 @@ print(f"{num1} ^ {num2} = {num1**num2}")
 
 # try running this program a few times to see that both blocks of print statements are equivalent.
 
+# After you've run it, add a line to both sections that performs and prints subtraction.
 
-# Once you've run it a few times we are going to show how you can use a file for input instead of having the user enter input on the keyboard.
+
+
+# Next we are going to show how you can use a file for input instead of having the user enter input on the keyboard.
 # Take a look at the files input1.txt and input2.txt in this directory.
 # Each of these files has two numbers in it.
 # 
